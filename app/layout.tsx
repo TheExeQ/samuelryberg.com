9e2b988e8741fbb93bd7bbc0ff445331fff7bac9
Samuel Ryberg <samuel.ryberg@gmail.com>
2024-03-25 20:39:57 +0100

    chore: added more metadata to improve SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,24 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Samuel Ryberg",
-  description: "Personal developer portfolio website",
+  title: "Samuel Ryberg | Official Website",
+  description: "Explore Samuel Ryberg's projects, skills, and experience.",
+  keywords: [
+    "Samuel Ryberg",
+    "Game",
+    "Developer",
+    "Programmer",
+    "Engineer",
+    "Portfolio",
+    "C++",
+    "C#",
+    "Unity",
+    "Unreal Engine",
+  ],
+  authors: [
+    { name: "Samuel Ryberg", url: "https://www.linkedin.com/in/samuelryberg/" },
+    { name: "ExeQ", url: "https://github.com/theexeq" },
+  ],
 };
 
 export default function RootLayout({
