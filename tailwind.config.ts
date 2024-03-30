58f727cf25036a87910745248b5d20a7aabca842
Samuel Ryberg <samuel.ryberg@gmail.com>
2024-03-30 16:00:13 +0100

    chore: small cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,10 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      primary: "#0F1116",
+      secondary: "#161413",
+      white: "#FFFFFF",
     },
   },
   plugins: [],
