bffc85d386e693546ba9708a292fefc7fa2a2521
Samuel Ryberg <samuel.ryberg@gmail.com>
2024-04-08 21:12:23 +0200

    test: learning next images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import Vipertrace from "@public/projects/vipertrace/vipertrace.png";
+
 export default function Home() {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="relative h-screen flex justify-center items-center overflow-hidden">
+      <Image
+        alt="Vipertrace"
+        src={Vipertrace}
+        quality={100}
+        fill
+        priority
+        sizes="(max-width: 1080px) 100vw"
+        style={{ objectFit: "cover" }}
+      />
       <a
-        className="font-bold text-5xl underline"
+        className="font-bold text-5xl text-center underline z-10"
         href="https://samuelryberg.com/"
       >
         Samuel Ryberg Portfolio
