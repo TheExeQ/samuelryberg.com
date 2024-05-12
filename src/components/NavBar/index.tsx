4f6810a09acb4e0fc6a7dffc2af3377db5746be7
Samuel Ryberg <samuel.ryberg@gmail.com>
2024-05-12 23:57:00 +0200

    feat: added temp 404 page

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { FC } from "react";
 
 const NavBar: FC = () => (
-  <div className="fixed w-full p-8 flex items-center justify-between z-10">
+  <div className="fixed left-0 top-0 w-full p-8 flex items-center justify-between z-10">
     <a
       className="bg-gradient-to-r from-purple-500 to-pink-200 bg-clip-text text-transparent text-2xl font-bold uppercase"
       href="https://samuelryberg.com/"
