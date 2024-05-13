a4a327af97ce5026ec17298b5ed59f3a7d3d505b
Samuel Ryberg <samuel.ryberg@gmail.com>
2024-05-13 22:34:04 +0200

    feat: added react icons and switched out nav items for menu icon

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,35 +1,19 @@
 import Link from "next/link";
 import { FC } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const NavBar: FC = () => (
-  <div className="fixed left-0 top-0 w-full p-8 flex items-center justify-between z-10">
-    <a
-      className="bg-gradient-to-r from-purple-500 to-pink-200 bg-clip-text text-transparent text-2xl font-bold uppercase"
-      href="https://samuelryberg.com/"
+  <nav className="fixed left-0 top-0 w-full p-8 flex items-center justify-between z-10">
+    <Link
+      className="bg-gradient-to-r from-purple-500 to-pink-200 bg-clip-text text-transparent text-2xl font-bold uppercase cursor-pointer"
+      href="/"
     >
-      SR
-    </a>
-    <div className="flex items-center">
-      <Link
-        className="mx-2 text-l uppercase hover:underline hover:font-semibold"
-        href="/projects"
-      >
-        Projects
-      </Link>
-      <Link
-        className="mx-2 text-l uppercase hover:underline hover:font-semibold"
-        href="/about"
-      >
-        About
-      </Link>
-      <Link
-        className="mx-2 text-l uppercase hover:underline hover:font-semibold"
-        href="/contact"
-      >
-        Contact
-      </Link>
-    </div>
-  </div>
+      SR.
+    </Link>
+    <button>
+      <AiOutlineMenu size={28} />
+    </button>
+  </nav>
 );
 
 export default NavBar;
