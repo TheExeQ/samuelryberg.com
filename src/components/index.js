4d853bf6bb472048e3acd1dc04ec4f6ec6abdaca
Samuel Ryberg <samuel.ryberg@gmail.com>
2024-09-16 21:02:19 +0200

    refactor: moved nav to layout folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "./ui/themeProvider";
 import { ModeToggle } from "./ui/modeToggle";
-import { FloatingNav } from "./ui/floatingNav";
+import { FloatingNav } from "./layout/floatingNav";
 import { Footer } from "./layout/footer";
 import { Spotlight } from "./ui/spotlight";
 import { ContactForm } from "./contactForm";
