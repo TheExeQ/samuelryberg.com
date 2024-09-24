51579bdeb420523883f8471e4c8cdb16a973d7e8
Samuel Ryberg <samuel.ryberg@gmail.com>
2024-09-24 22:01:12 +0200

    style: added background to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,8 +3,12 @@ import { FC } from "react";
 import { ContactForm } from "@/components";
 
 const Contact: FC = () => (
-  <div className="flex h-screen items-center justify-center">
-    <ContactForm />
+  <div className="relative min-h-screen">
+    <div className="absolute inset-0 -z-10 flex h-auto w-full items-center justify-center bg-grid-black/[0.05] dark:bg-grid-white/[0.05]" />
+    <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
+    <div className="flex h-screen items-center justify-center">
+      <ContactForm />
+    </div>
   </div>
 );
 
