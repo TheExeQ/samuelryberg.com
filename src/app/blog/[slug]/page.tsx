e635c06bc5480532c4041763fd52d92cbcff0d15
Samuel Ryberg <samuel.ryberg@gmail.com>
2025-01-03 17:59:38 +0100

    fix: small improvements

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -11,33 +11,38 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function Blog({ params }: { params: any }) {
-  let post = getBlogPosts().find((post) => post.slug === params.slug);
+export default async function Blog({ params }: { params: any }) {
+  const { slug } = await params;
+  let post = getBlogPosts().find((post) => post.slug === slug);
 
   if (!post) {
     notFound();
   }
 
   return (
-    <article className="mx-auto mb-16 mt-32 flex min-h-screen max-w-[800px] flex-col items-start justify-center px-4">
-      <div className="dark:prose-dark prose grid flex-1 grid-cols-1 md:grid-cols-[1fr,minmax(auto,640px),1fr] md:[&>*]:col-start-2">
-        <div>
-          <header className="w-full">
-            <h1 className="text-foreground">{post.metadata.title}</h1>
-            <div className="mt-2 flex w-full flex-col items-start justify-between md:flex-row md:items-center">
-              <div className="flex items-center">
-                <time
-                  className="text-sm text-muted-foreground"
-                  dateTime={new Date(post.metadata.publishedAt).toUTCString()}
-                >
-                  Samuel Ryberg / {parseISO(post.metadata.publishedAt)}
-                </time>
+    <section>
+      <div className="mx-auto mb-16 mt-32 flex min-h-screen max-w-[800px] flex-col items-start justify-center px-4">
+        <div className="dark:prose-dark prose grid flex-1 grid-cols-1 md:grid-cols-[1fr,minmax(auto,640px),1fr] md:[&>*]:col-start-2">
+          <div>
+            <header className="w-full">
+              <h1 className="text-foreground">{post.metadata.title}</h1>
+              <div className="mt-2 flex w-full flex-col items-start justify-between md:flex-row md:items-center">
+                <div className="flex items-center">
+                  <time
+                    className="text-sm text-muted-foreground"
+                    dateTime={new Date(post.metadata.publishedAt).toUTCString()}
+                  >
+                    Samuel Ryberg / {parseISO(post.metadata.publishedAt)}
+                  </time>
+                </div>
               </div>
-            </div>
-          </header>
-          <MDXComponent source={post.content} />
+            </header>
+            <article>
+              <MDXComponent source={post.content} />
+            </article>
+          </div>
         </div>
       </div>
-    </article>
+    </section>
   );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,7 +5,6 @@ import { ContactForm } from "@/components";
 const Contact: FC = () => (
   <div className="relative min-h-screen">
     <div className="absolute inset-0 -z-10 flex h-auto w-full items-center justify-center bg-grid-black/[0.05] dark:bg-grid-white/[0.05]" />
-    <div className="pointer-events-none absolute inset-0 hidden items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black md:flex" />
     <div className="flex h-screen items-center justify-center">
       <ContactForm />
     </div>
