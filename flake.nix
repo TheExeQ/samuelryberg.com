9cfa87cfcf750dd484d9b360e1ff0566478f2329
Samuel Ryberg <26822321+TheExeQ@users.noreply.github.com>
2026-08-23 19:19:10 +0200

    feat: added nix flake for isolated dev shell

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Portfolio Website Project - Nix Develop Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs_22
+              pkgs.pnpm
+            ];
+          };
+        }
+      );
+    };
+}
